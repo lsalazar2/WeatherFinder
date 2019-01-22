1fbd761bf2664aed43089b0df3ed9f66d0526df6
Missy Leathers <missy@avleathers.com>
2019-01-22 11:02:03 -0800

    Getting everything

---
+++ models/blogger.js
@@ -0,0 +1,13 @@
+module.exports = function(sequelize, DataTypes) {
+    var Blogger = sequelize.define("Blogger", {
+            name: DataTypes.STRING
+    });
+ 
+    Blogger.associate = function(bloggings) {
+     Blogger.hasMany(bloggings.Post, {
+        onDelete: "cascade"
+      });
+    };
+ 
+    return Blogger;
+  };
